db0965fb34bfe5e75a1e13dc16e442e041cfc077
Jens <jenstheminer@gmail.com>
2024-08-29 15:57:16 +0200

    Yesyesyes

---
+++ src/main/java/kippie/me/DiscordBotMinecraft/Main.java
@@ -0,0 +1,45 @@
+package kippie.me.DiscordBotMinecraft;
+
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.security.auth.login.LoginException;
+
+public final class Main extends JavaPlugin {
+
+    private ShardManager shardManager;
+
+    public Main() throws LoginException, InterruptedException {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault();
+        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+        builder.setActivity(Activity.watching("Hi :)"));
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.MESSAGE_CONTENT);
+        shardManager = builder.build();
+        shardManager.addEventListener();
+    }
+
+    public static void main(String[] args) {
+        try {
+            Main bot = new Main();
+        } catch (LoginException e) {
+            System.out.println("Bot token invalid!");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
